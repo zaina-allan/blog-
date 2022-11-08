b8d2cdf532014312d78649c940b4fbbbbc8b8ad1
zaina-allan <75915075+zaina-allan@users.noreply.github.com>
2022-11-08 18:40:15 +0200

    Add files via upload

---
+++ core/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+
+# Register your models here.
+from django.contrib import admin
+from core.models import Blog
+@admin.register(Blog)
+class BlogAdmin(admin.ModelAdmin):
+    pass
